524e29fba6a7e8013c0c7a80357f244d268cc892
BinaryBard <shrikhandeaniruddha@gmail.com>
2023-02-05 18:05:30 +0530

    1D Array

---
+++ p59.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class p59 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+        while(test>0){
+            int size = sc.nextInt();
+            int[] array = new int[size];
+            for (int i = 0; i<size; i++){
+                array[i]= sc.nextInt();
+            }
+            int key = sc.nextInt();
+            for (int i = 0; i<size; i++) {
+                if (array[i] == key) {
+                    System.out.println("1");
+                }
+                else {
+                    System.out.println("0");
+                }
+            }
+            test--;
+
+
+        }
+
+
+
+    }
+}
